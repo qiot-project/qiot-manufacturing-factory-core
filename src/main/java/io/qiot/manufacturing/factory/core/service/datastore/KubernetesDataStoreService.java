9aa10e22f54b8bb5ed01547a7a4ee8ceb764ecbd
Andrea Battaglia <andreabattaglia@users.noreply.github.com>
2021-11-13 12:26:31 +0100

    fix read UUID from secret

--- src/main/java/io/qiot/manufacturing/factory/core/service/datastore/KubernetesDataStoreService.java
+++ src/main/java/io/qiot/manufacturing/factory/core/service/datastore/KubernetesDataStoreService.java
@@ -3,14 +3,19 @@
  */
 package io.qiot.manufacturing.factory.core.service.datastore;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.Map;
 import java.util.UUID;
+import java.util.Map.Entry;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Typed;
 
 import org.slf4j.Logger;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.fabric8.kubernetes.api.model.Secret;
 import io.fabric8.kubernetes.api.model.SecretBuilder;
@@ -33,12 +38,14 @@ public class KubernetesDataStoreService implements DataStoreService {
     public static final String FACTORY_NAME = "name";
 
     final Logger LOGGER;
+    final ObjectMapper MAPPER;
     final SecretOperation secretOperation;
 
-    public KubernetesDataStoreService(Logger LOGGER,
+    public KubernetesDataStoreService(Logger LOGGER, ObjectMapper MAPPER,
             SecretOperation secretOperation) {
         super();
         this.LOGGER = LOGGER;
+        this.MAPPER = MAPPER;
         this.secretOperation = secretOperation;
     }
 
@@ -51,14 +58,17 @@ public class KubernetesDataStoreService implements DataStoreService {
         Secret factoryDataSecret = new SecretBuilder().withNewMetadata()
                 .withName(SECRET_NAME).endMetadata()
                 .addToData(FACTORY_ID,
-                        Base64.getEncoder().encodeToString(
-                                factoryData.id.toString().getBytes()))//
+                        Base64.getEncoder()
+                                .encodeToString(factoryData.id.toString()
+                                        .getBytes(StandardCharsets.UTF_8)))//
                 .addToData(FACTORY_SERIAL,
                         Base64.getEncoder()
-                                .encodeToString(factoryData.serial.getBytes()))//
+                                .encodeToString(factoryData.serial
+                                        .getBytes(StandardCharsets.UTF_8)))//
                 .addToData(FACTORY_NAME,
                         Base64.getEncoder()
-                                .encodeToString(factoryData.name.getBytes()))//
+                                .encodeToString(factoryData.name
+                                        .getBytes(StandardCharsets.UTF_8)))//
                 .build();
         secretOperation.operation().create(factoryDataSecret);
     }
@@ -75,13 +85,33 @@ public class KubernetesDataStoreService implements DataStoreService {
         Secret factoryDataSecret = secretResource.get();
         Map<String, String> secretData = factoryDataSecret.getData();
 
+        try {
+            LOGGER.info("Data contained in the secret {}: \n{}", SECRET_NAME,
+                    MAPPER.writerWithDefaultPrettyPrinter()
+                            .writeValueAsString(secretData));
+        } catch (JsonProcessingException e) {
+            LOGGER.info("Data contained in the secret {}: ", SECRET_NAME);
+            for (Entry<String, String> entry : secretData.entrySet()) {
+                LOGGER.info("\"{}\" : \"{}\"", entry.getKey(),
+                        entry.getValue());
+            }
+        }
+
         factoryData = new FactoryDataDTO();
-        factoryData.id = UUID.nameUUIDFromBytes(
-                Base64.getDecoder().decode(secretData.get(FACTORY_ID)));
-        factoryData.serial = new String(
-                Base64.getDecoder().decode(secretData.get(FACTORY_SERIAL)));
-        factoryData.name = new String(
-                Base64.getDecoder().decode(secretData.get(FACTORY_NAME)));
+        factoryData.id = UUID.fromString(new String(Base64.getDecoder().decode(
+                secretData.get(FACTORY_ID).getBytes(StandardCharsets.UTF_8))));
+        factoryData.serial = new String(Base64.getDecoder().decode(secretData
+                .get(FACTORY_SERIAL).getBytes(StandardCharsets.UTF_8)));
+        factoryData.name = new String(Base64.getDecoder().decode(
+                secretData.get(FACTORY_NAME).getBytes(StandardCharsets.UTF_8)));
+        try {
+            LOGGER.info("Decrypted data contained in the secret {}: \n{}",
+                    SECRET_NAME, MAPPER.writerWithDefaultPrettyPrinter()
+                            .writeValueAsString(factoryData));
+        } catch (JsonProcessingException e) {
+            LOGGER.info("Data contained in the secret {}: {}", SECRET_NAME,
+                    factoryData);
+        }
         LOGGER.info("Data loaded successfully: {}", factoryData);
         return factoryData;
     }
